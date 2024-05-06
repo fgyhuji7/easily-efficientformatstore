22531dae2abbdebc4b19f5b586f68e5e1b3affa9
fgyhuji7 <js1hichamsf@hotmail.com>
2024-05-06 22:41:40 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function serialize(root) {
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    if (node) {
-      result.push(node.val);
-      queue.push(node.left, node.right);
-    } else {
-      result.push(null);
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
-  while (result[result.length - 1] === null) {
-    result.pop();
-  }
-  return result;
+  return currMax;
 }
